8790df2ce1aafa532acbae2a5914100ada1f1428
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-09-09 09:28:59 -0700

    add and pass first two tests, instantiateCorrectly and getsGuests

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,12 @@
 public class Event {
+  private int mGuests;
+
+  public Event (int guests, String food, String beverages, String entertainment) {
+    mGuests = guests;
+  }
+
+  public int getGuests() {
+    return mGuests;
+  }
 
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,10 +1,17 @@
 import org.junit.*;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 public class EventTest {
 
   @Test
   public void newEvent_instantiatesCorrectly() {
-    Event
+    Event testEvent = new Event(10, "Light Refreshment", "Non-Alcoholic", "None");
+    assertEquals(true, testEvent instanceof Event);
+  }
+
+  @Test
+  public void getGuests_getGuests_10() {
+    Event testEvent = new Event(10, "Light Refereshment", "Non-Alcholic", "None");
+    assertEquals(10, testEvent.getGuests());
   }
 }
