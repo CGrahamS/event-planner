de9c9ca3fe2a0ab2b0031530f7a3fa75495e98ac
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-09-09 10:50:43 -0700

    add passing tests 5 getsEntertainment, 6 calculatesCost80, and 7 calculatesCost1400

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,11 +2,14 @@ public class Event {
   private int mGuests;
   private String mFood;
   private String mBeverage;
+  private String mEntertainment;
+  private int mPrice = 0;
 
-  public Event (int guests, String food, String beverages, String entertainment) {
+  public Event(int guests, String food, String beverages, String entertainment) {
     mGuests = guests;
     mFood = food;
-    mBeverage= beverages;
+    mBeverage = beverages;
+    mEntertainment = entertainment;
   }
 
   public int getGuests() {
@@ -21,5 +24,27 @@ public class Event {
     return mBeverage;
   }
 
+  public String getEntertainment() {
+    return mEntertainment;
+  }
+
+  public int calculateCost() {
+    if (mFood.equalsIgnoreCase("Light Refreshment")) {
+      mPrice += (3 * mGuests);
+    } else if (mFood.equalsIgnoreCase("Hors d'oevures")) {
+      mPrice += (5 * mGuests);
+    }
+    if (mBeverage.equalsIgnoreCase("Non-Alcoholic")) {
+      mPrice += (5 * mGuests);
+    } else if (mBeverage.equalsIgnoreCase("Beer & Wine")) {
+      mPrice += (20 * mGuests);
+    }
+    if (mEntertainment.equalsIgnoreCase("None")) {
+      mPrice += 0;
+    } else if (mEntertainment.equalsIgnoreCase("DJ")) {
+      mPrice += 150;
+    }
+    return mPrice;
+  }
 
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -22,8 +22,26 @@ public class EventTest {
   }
 
   @Test
-  public void getBeverages_getsBeverage_NonAlcholic() {
+  public void getBeverage_getsBeverage_NonAlcholic() {
     Event testEvent = new Event(10, "Light Refreshment", "Non-Alcoholic", "None");
     assertEquals("Non-Alcoholic", testEvent.getBeverage());
+  }
+
+  @Test
+  public void getEntertainment_getsEntertainment_None() {
+    Event testEvent = new Event(10, "Light Refreshment", "Non-Alcoholic", "None");
+    assertEquals("None", testEvent.getEntertainment());
+  }
+
+  @Test
+  public void  calculateCost_calculatesCostOfEvent_80() {
+    Event testEvent = new Event(10, "Light Refreshment", "Non-Alcoholic", "None");
+    assertEquals(80, testEvent.calculateCost());
+  }
+
+  @Test
+  public void calculateCost_calculatesCostOfEvent_1400() {
+    Event testEvent = new Event(50, "Hors d'oevures", "Beer & Wine", "DJ");
+    assertEquals(1400, testEvent.calculateCost());
   }
 }
